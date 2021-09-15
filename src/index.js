e49327cf5820834a14135a8c5f38a0abb3ccef30
tauqeer-ahmad-gh <tauqeer@agaze.in>
2021-09-15 23:50:58 +0530

    setup socket support for the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+const http = require('http')
 // 3rd party modules
 const express = require('express');
 // built-in modules
 const path = require('path');
+// socket library
+const socketio = require('socket.io')
 
 // initialise the app
 const app = express();
+
+// http module create server
+const server = http.createServer(app)
+
+// initialise socket into server
+const io = socketio(server)
 
 // configure the app
 const port = process.env.PORT || 3000;
@@ -13,7 +22,11 @@ const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirPath));
 
+io.on('connection', () => {
+    console.log("new socket connection is up!")
+})
+
 // kickstart the server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server is running on ${port}!`);
 })
